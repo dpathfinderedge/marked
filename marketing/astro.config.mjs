74b2b8665a795335e2e6aa2c2599e2eea5fa9748
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2026-08-29 17:44:15 +0100

    wire site & app url

--- marketing/astro.config.mjs
+++ marketing/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://maarked.vercel.app",
   integrations: [
     tailwind({
       applyBaseStyles: false,
--- marketing/src/lib/constants.ts
+++ marketing/src/lib/constants.ts
@@ -1,1 +1,1 @@
-export const APP_URL = "https://YOUR-APP-URL.vercel.app";
+export const APP_URL = "https://app-marked.vercel.app";
